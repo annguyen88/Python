f6795b5193e919c82b6f28032fd1de5cc27f52b1
Andrew Nguyen <anguyen@hubcitymedia.com>
2015-07-16 16:03:06 -0400

    adding popups

---
+++ Python2.7/popWindow.py
@@ -0,0 +1,7 @@
+
+from easygui import *
+
+msgbox("My First Popup!", title="Andrew Nguyen Display Window")
+
+choices = ["Yes","No","Only on Friday"]
+reply = choicebox("Do you like to eat fish?", choices=choices)
